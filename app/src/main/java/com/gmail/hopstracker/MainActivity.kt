86f7959fcf8ef47bf6a22ea18922c2053fa6dd9e
TaylorMarieMusic <taylormariemusic2@gmail.com>
2021-05-27 22:04:32 -0400

    ok

--- app/src/main/java/com/gmail/hopstracker/MainActivity.kt
+++ app/src/main/java/com/gmail/hopstracker/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //try again again again
+        //try again again again again
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
